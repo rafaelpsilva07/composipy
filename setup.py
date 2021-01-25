338a5af39a76ef606168fcec7307af71ff22af6d
Rafael <rafaelpsilva07@gmail.com>
2021-01-25 20:30:13 -0300

    Setup added

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mimo_composipy", # Replace with your own username
+    version="0.0.1",
+    author="Rafael Pereira",
+    author_email="rafaelpsilva07@gmail.com",
+    description="This package intends to perform composite material calculations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rafaelpsilva07/mimo_composipy.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
