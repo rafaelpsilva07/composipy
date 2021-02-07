a7ef075512dc8728fd1bad937247113995bedde0
Rafael <rafaelpsilva07@gmail.com>
2021-02-07 19:49:11 -0300

    bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mimo_composipy", # Replace with your own username
-    version="0.1.1",
+    version="0.1.2",
     author="Rafael Pereira",
     author_email="rafaelpsilva07@gmail.com",
     description="This package intends to perform composite material calculations",
