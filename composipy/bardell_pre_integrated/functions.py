66f1e5d0fcf6b848dc6f3156e72611ab987110b5
Rafael <rafaelpsilva07@gmail.com>
2023-01-15 19:01:30 -0300

    enh: studies of pre integration

--- composipy/bardell_pre_integrated/functions.py
+++ composipy/bardell_pre_integrated/functions.py
@@ -2,9 +2,10 @@
 import sys
 sys.path.append('D:/repositories/composipy/composipy/bardell_pre_integrated')
 from _fxi import *
+from _K_KG_IJKL import * 
 
 
-__all__ = ['i_d_fxi', 'i_dd_fxi']
+__all__ = ['i_d_fxi', 'i_dd_fxi', 'calc_k']
 
 
 def fxi(i, xi):
@@ -79,3 +80,19 @@ def i_dd_fxi(ij, lbound, ubound):
     '''
     definite_integral = _i_dd_fxi(ij, ubound) - _i_dd_fxi(ij, lbound)
     return definite_integral
+
+
+def calc_k(F, i, j, a, b):
+    A11, A12, A16, A22, A26, A66 = F[0, 0], F[0, 1], F[0, 2], F[1, 1], F[1, 2], F[2, 2]
+    B11, B12, B16, B22, B26, B66 = F[0, 3], F[0, 4], F[0, 5], F[1, 4], F[1, 5], F[2, 5]
+    D11, D12, D16, D22, D26, D66 = F[3, 3], F[3, 4], F[3, 5], F[4, 4], F[4, 5], F[5, 5]
+
+# F = sp.Matrix([
+#     [A11, A12, A16, B11, B12, B16],
+#     [A12, A22, A26, B12, B22, B26],
+#     [A16, A26, A66, B16, B26, B66],
+#     [B11, B12, B16, D11, D12, D16],
+#     [B12, B22, B26, D12, D22, D26],
+#     [B16, B26, B66, D16, D26, D66]])
+    kij = eval(K_IJKL[(i, j)])
+    return kij
--- composipy/bardell_pre_integrated/write_bardell_terms.py
+++ composipy/bardell_pre_integrated/write_bardell_terms.py
@@ -3,58 +3,47 @@ import numpy as np
 import pickle
 
 n_terms = 10
-xi = sp.symbols('xi')
+xi, eta, a, b = sp.symbols(['xi', 'eta', 'a', 'b'])
 f1 = 1/2 - 3/4*xi + 1/4*xi**3
 f2 = 1/8 - 1/8*xi - 1/8*xi**2 + 1/8*xi**3
 f3 = 1/2 + 3/4*xi - 1/4*xi**3
 f4 = -1/8 - 1/8*xi + 1/8*xi**2 + 1/8*xi**3
 
-f_xi = {
-    0: f1,
-    1: f2,
-    2: f3,
-    3: f4
-} # first terms of set of functions
+g1 = 1/2 - 3/4*eta + 1/4*eta**3
+g2 = 1/8 - 1/8*eta - 1/8*eta**2 + 1/8*eta**3
+g3 = 1/2 + 3/4*eta - 1/4*eta**3
+g4 = -1/8 - 1/8*eta + 1/8*eta**2 + 1/8*eta**3
 
-d_f_xi = {
-    0: sp.diff(f_xi[0], xi), 
-    1: sp.diff(f_xi[1], xi),
-    2: sp.diff(f_xi[2], xi),
-    3: sp.diff(f_xi[3], xi)
-}
+f_xi = {0: f1, 1: f2, 2: f3, 3: f4} # first terms of set of functions
+g_eta = {0: g1, 1: g2, 2: g3, 3: g4}
 
-dd_f_xi = {
-    0: sp.diff(f_xi[0], xi, xi), 
-    1: sp.diff(f_xi[1], xi, xi),
-    2: sp.diff(f_xi[2], xi, xi),
-    3: sp.diff(f_xi[3], xi, xi)
-}
 
-def _orth_polynomials(r):   
+def _orth_polynomials(r, sym):   
     '''
     r term from the set of functions used by Bardell (1991)
 
     Parameters
     ----------
     r : int
+    sym : sympy.symbol
+        symbol to be used in equation
 
     Returns
     -------
-    f_xi_r : sympy equation
+    f_sym_r : sympy equation
     r term from the set of functions.
     '''
-    global xi
     nmax = r//2 + 1 #denotes its own integer part (Bardel, 1991).
-    f_xi_r = 0
+    f_sym_r = 0
 
     for n in range(0, nmax):
-        f_xi_r += (
+        f_sym_r += (
             (-1)**n * sp.factorial2(2*r-2*n-7)
             / (2**n * sp.factorial(n) * sp.factorial(r-2*n-1))
-            * xi**(r-2*n-1)
+            * sym**(r-2*n-1)
             )
-    f_xi_r = sp.sympify(f_xi_r)
-    return f_xi_r
+    f_sym_r = sp.sympify(f_sym_r)
+    return f_sym_r
 
 
 if __name__ == '__main__':
@@ -63,9 +52,26 @@ if __name__ == '__main__':
 
     # Complete set of bardell functions
     for i in range(4, n_terms):
-        f_xi[i] = _orth_polynomials(i+1)
-        d_f_xi[i] = sp.diff(f_xi[i], xi)
-        dd_f_xi[i] = sp.diff(f_xi[i], xi, xi)
+        f_xi[i] = _orth_polynomials(i+1, xi)
+        g_eta[i] = _orth_polynomials(i+1, eta)
+
+    S = {}
+    B11, B22, B31, B32, B43, B53, B63 = {}, {}, {}, {}, {}, {}, {}
+    for i, fi in f_xi.items():
+        for j, gj in g_eta.items():
+            S[(i, j)] = f_xi * g_eta
+            B11[(i, j)] = (2/a) * sp.diff(S[(i, j)], xi)
+            B22[(i, j)] = (2/b) * sp.diff(S[(i, j)], eta)
+            B31[(i, j)] = (2/b) * sp.diff(S[(i, j)], eta)
+            B32[(i, j)] = (2/a) * sp.diff(S[(i, j)], xi)
+            B43[(i, j)] = -(4/a**2) * sp.diff(S[(i, j)], xi, xi)
+            B53[(i, j)] = -(4/b**2) * sp.diff(S[(i, j)], eta, eta)
+            B63[(i, j)] = -2 * (4/(a*b)) * sp.diff(S[(i, j)], xi, eta)
+
+   
+
+
+
 
     # Converting simbols into strings
     txt_fxi = 'FXI = [\n'
