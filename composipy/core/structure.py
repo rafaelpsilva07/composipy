f50e593cd5d1849865e0d602d7be68fb1354746e
Rafael <rafaelpsilva07@gmail.com>
2023-04-02 16:51:17 -0300

    ENH: computed_constraints removed from PlateStruct

--- composipy/core/structure.py
+++ composipy/core/structure.py
@@ -11,7 +11,7 @@ from composipy.core.property import Property
 from composipy.utils import ComposipyValidator
 from composipy.pre_integrated_component.build_k import *
 from composipy.pre_integrated_component.functions import sxieta
-
+from composipy.core.structures import _generate_index
 
 class Structure(ComposipyValidator):
       pass
@@ -136,99 +136,6 @@ class PlateStructure(Structure):
     def n(self):
             return self.__n
 
-
-    def _compute_constraints(self):
-        if self.constraints == 'PINNED':
-            x0 = ['TX', 'TY', 'TZ']
-            xa = ['TX', 'TY', 'TZ']
-            y0 = ['TX', 'TY', 'TZ']
-            yb = ['TX', 'TY', 'TZ']
-        elif self.constraints == 'CLAMPED':
-            x0 = ['TX', 'TY', 'TZ', 'RX', 'RY', 'RZ']
-            xa = ['TX', 'TY', 'TZ', 'RX', 'RY', 'RZ']
-            y0 = ['TX', 'TY', 'TZ', 'RX', 'RY', 'RZ']
-            yb = ['TX', 'TY', 'TZ', 'RX', 'RY', 'RZ']
-        else:
-            x0 = self.constraints['x0']
-            xa = self.constraints['xa']
-            y0 = self.constraints['y0']
-            yb = self.constraints['yb']
-
-        sm = [i for i in range(self.m+4)]
-        sn = [i for i in range(self.n+4)]
-
-        um, un = sm.copy(), sn.copy()
-        vm, vn = sm.copy(), sn.copy()
-        wm, wn = sm.copy(), sn.copy()
-
-        # x0
-        if 'TX' in x0:
-            um.remove(0)
-        if 'TY' in x0:
-            vm.remove(0)
-        if 'TZ' in x0:
-            wm.remove(0)
-        if 'RX' in x0:
-            um.remove(1)
-        if 'RY' in x0:
-            vm.remove(1)
-        if 'RZ' in x0:
-            wm.remove(1)
-        #xa
-        if 'TX' in xa:
-            um.remove(2)
-        if 'TY' in xa:
-            vm.remove(2)
-        if 'TZ' in xa:
-            wm.remove(2)
-        if 'RX' in xa:
-            um.remove(3)
-        if 'RY' in xa:
-            vm.remove(3)
-        if 'RZ' in xa:
-            wm.remove(3)
-        
-        #y0
-        if 'TX' in y0:
-            un.remove(0)
-        if 'TY' in y0:
-            vn.remove(0)
-        if 'TZ' in y0:
-            wn.remove(0)
-        if 'RX' in y0:
-            un.remove(1)
-        if 'RY' in y0:
-            vn.remove(1)
-        if 'RZ' in y0:
-            wn.remove(1)
-        
-        #yb
-        if 'TX' in yb:
-            un.remove(2)
-        if 'TY' in yb:
-            vn.remove(2)
-        if 'TZ' in yb:
-            wn.remove(2)
-        if 'RX' in yb:
-            un.remove(3)
-        if 'RY' in yb:
-            vn.remove(3)
-        if 'RZ' in yb:
-            wn.remove(3)
-        
-        um, un = um[0:self.m], un[0:self.n]
-        vm, vn = vm[0:self.m], un[0:self.n]
-        wm, wn = wm[0:self.m], un[0:self.n]
-
-        uidx = list(product(um, un, um, un))
-        vidx = list(product(vm, vn, vm, vn))
-        widx = list(product(wm, wn, wm, wn))
-
-        self.su_idx = list(product(um, un))
-        self.sv_idx = list(product(vm, vn))
-        self.sw_idx = list(product(wm, wn))
-
-        return (uidx, vidx, widx)
     
     #TODO: parallel process, sparse matrix, cython
     def calc_K_KG(self):
@@ -251,7 +158,7 @@ class PlateStructure(Structure):
         k11, k12, k13, k21, k22, k23, k31, k32, k33 = [], [], [], [], [], [], [], [], []
         k33g = []
 
-        uidx, vidx, widx = self._compute_constraints()
+        uidx, vidx, widx = _generate_index(self.constraints, self.m, self.n)
 
         for i in range(self.m**2*self.n**2):
                 ui, uj, uk, ul = uidx[i]
