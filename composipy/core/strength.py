8ac298f0d72bb50dee4cc1be14e78793342caf22
Rafael <rafaelpsilva07@gmail.com>
2024-09-22 14:07:18 -0300

    enh: calculation of plies strains

--- composipy/core/strength.py
+++ composipy/core/strength.py
@@ -41,3 +41,49 @@ class LaminateStrength():
         self.My = My
         self.Mxy = Mxy
 
+
+    def epsilon0(self):
+        '''
+        Calculates the strains of the laminate
+        Returns
+        -------
+        epsilon0: numpy ndarray
+            [epsilonx0, epsilony0, epsilonxy0, kappax0, kappay0, kappaxy0]
+        '''
+
+        ABD = self.dproperty.ABD
+        abd = np.linalg.inv(ABD) #equivalent relations on pg 153 of Daniel
+        N = np.array([Nx, Ny, Nxy, Mx, My, Mxy])
+        return abd @ N
+
+
+    def epsilonk(self):
+        '''
+        Calculates the strains for each lamina
+        Returns
+        -------
+        epsilonk: list
+            [epsilon1, epsilon2, ..., epsilonk], with epsilon as np.ndarray()
+            For each epsilon we have
+            epsilonk = np.ndarray([epsilonx, epsilony, gammas])k, coordinate directions
+            For reference refer to page 145 of Daniel equation 5.8
+        '''       
+        nplies = len(self.dproperty.stacking)
+        z = self.dproperty.z_position
+        zmid = [(z[i]+z[i+1]) / 2
+               for i in range(nplies)]           
+        epsilon0 = self.epsilon0
+        epsilon = np.array([self.epsilon0[0],
+                            self.epsilon0[1],
+                            self.epsilon0[2],])
+        kappa = np.array([self.epsilon0[3],
+                          self.epsilon0[4],
+                          self.epsilon0[5],])        
+        epsilonk = []
+        for h in zmid:
+            epsilonk.append(
+                epsilon + h * kappa
+            )
+        return epsilonk
+        
+
